3fd3a8851519cab7ff1affec1bb2a9f65cd96caf
wesblake@proton.me <wesblake@proton.me>
2026-03-17 10:56:18 -0700

    Corrected logic to detect file I want, ignore the rest.

--- invoice_organizer.py
+++ invoice_organizer.py
@@ -6,28 +6,27 @@ def get_data() -> tuple[tuple[str],tuple[str]]:
     DOWNLOADS = Path.home() / "Downloads"
     WHITE_LIST = ["Invoice Number", "Status"]
     for i in DOWNLOADS.iterdir():
-        if not i.name.startswith("My Invoice") and not i.name.endswith(".csv"):
-            continue
-        # Create Dataframe.
-        df = pd.read_csv(i)
-        # Last row isn't needed.
-        df = df.drop(df.index[-1])
-        # Get rid of columns.
-        df = df[WHITE_LIST]
-        # Cast invoice numbers to int then to str.
-        df[WHITE_LIST[0]] = df[WHITE_LIST[0]].apply(lambda x: str(int(x)))
-        # Check duplicates, if their are, crash.
-        duplicates = df[df[WHITE_LIST[0]].duplicated(keep=False)]
-        if not duplicates.empty:
-            print(f"Duplicates!: {duplicates}")
-            SystemExit(1)
-        # Get paid invoices
-        paid_df = df[df[WHITE_LIST[1]] == "Paid"]
-        paid_tuple = tuple(paid_df[WHITE_LIST[0]])
-        # Everything else is with manager.
-        manager_df = df[df[WHITE_LIST[1]] != "Paid"]
-        manager_tuple = tuple(manager_df[WHITE_LIST[0]])
-        break
+        if i.name.startswith("My Invoice") and i.name.endswith(".csv"):
+            # Create Dataframe.
+            df = pd.read_csv(i)
+            # Last row isn't needed.
+            df = df.drop(df.index[-1])
+            # Get rid of columns.
+            df = df[WHITE_LIST]
+            # Cast invoice numbers to int then to str.
+            df[WHITE_LIST[0]] = df[WHITE_LIST[0]].apply(lambda x: str(int(x)))
+            # Check duplicates, if their are, crash.
+            duplicates = df[df[WHITE_LIST[0]].duplicated(keep=False)]
+            if not duplicates.empty:
+                print(f"Duplicates!: {duplicates}")
+                SystemExit(1)
+            # Get paid invoices
+            paid_df = df[df[WHITE_LIST[1]] == "Paid"]
+            paid_tuple = tuple(paid_df[WHITE_LIST[0]])
+            # Everything else is with manager.
+            manager_df = df[df[WHITE_LIST[1]] != "Paid"]
+            manager_tuple = tuple(manager_df[WHITE_LIST[0]])
+            break
     else:
         print("No invoice doc found (.csv).")
         SystemExit(1)
